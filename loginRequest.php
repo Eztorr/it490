be063382b34e7a54e005ab41fea69ebbfec988e9
jbc526 <jbc5@njit.edu>
2026-02-16 12:30:23 -0500

    added files for login

---
+++ loginRequest.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+/**
+$request = array();
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+*/
+$request = array();
+$request ['type'] = "Login";
+$request ['username'] = $_POST['username'];
+$request ['password'] = $_POST['password'];
+$response = $client->send_request($request);
+
+$payload = json_encode($response);
+echo $payload;
+
