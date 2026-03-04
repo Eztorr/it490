678df3cfa62267b32c49787aa5f4e393f2d417ad
jbc526 <jbc5@njit.edu>
2026-03-04 12:14:14 -0500

    only testing, not perfect!

--- sample/index.php
+++ sample/index.php
@@ -21,12 +21,10 @@ if (!toValidateSessionTokens($_SESSION['token']))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6F" crossorigin="anonymous">
+    
     <style>
         body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
             margin: 0;
             font-family: Ariel, sans-serif;
             background-color: #f0f0f0;
@@ -37,10 +35,26 @@ if (!toValidateSessionTokens($_SESSION['token']))
         </style>
         </head>
         <body>
+            <nav class = "navbar navbar-expand-sm bg-light">
+                <div class = "container-fluid">
+                    <ul class = "navbar-nav me-auto mb-2 mb-sm-0">
+                        <li class = "nav-item">
+                            <a class = "nav-link" href = "index.php"> Home </a>
+                        </li>
+                        <li class = "nav-item">
+                            <a class = "nav-link" href = "#"> Link </a>
+                        </li>
+                        <li class = "nav-item">
+                            <a class = "nav-link" href = "#"> Link </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
             <h1> Welcome </h1>
 	    <form action="/app/logout.php" method = "POST">
-		<button type ="submit"> Log Out</button>
+		<button type ="submit" class="btn btn-danger"> Log Out</button>
 	</form>
 </body>
-    </head>
 </html>
--- sample/loginPage.php
+++ sample/loginPage.php
@@ -11,15 +11,27 @@ if (isset($_SESSION["error"])){
    <link rel = "stylesheet" href= "loginPage.css">
 <head> 
    <title> Log In </title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6F" crossorigin="anonymous">
+
 </head>
 <body>
-    <div class ="loginForm">
+
+    <div class ="container mt-5">
     <form id ="validateForm" action ="/app/loginRequest.php" method = "POST">
-        <h1> Log In</h1>
-        <input name = "email" id ="email" placeholder = "Email"/>
-        <input name = "password" id="password" type = "password" placeholder = "Password"/>
-        <button type = "submit"> Log In </button>
+        <h1 class ="text-center mb-4"> Log In</h1>
+
+        <div class = "mb-3">
+        <label for = "email" class = "form-label"> Email </label>
+        <input name = "email" id ="email" placeholder = " Enter Email" class="form-control"/>
+        </div>
+
+        <div class = "mb-3">
+        <label for = "password" class = "form-label"> Password </label>
+        <input name = "password" id="password" type = "password" placeholder = "Password" class="form-control"/>
+        </div>
+        <button type = "submit" class="btn btn-primary"> Log In </button>
     </form>  
+    
     <?php if (!empty($error)): ?>
         <p style = "color: black; margin-top:15px;"> 
             <?php echo $error; ?> </p>
